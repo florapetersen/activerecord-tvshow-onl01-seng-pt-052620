8a1975f07784784e77a5c814103db5297d567ccd
Flora Petersen <floramarina@comcast.net>
2020-08-16 21:11:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,13 @@ class Show < ActiveRecord::Base
     highest_rating = self.highest_rating
     Show.find_by(rating: highest_rating)
   end
+
+  def self.lowest_rating 
+    Show.minimum(:rating)
+  end 
+
+  def self.least_popular_show 
+    lowest_rating = self.lowest_rating 
+    Show.find_by(rating: lowest_rating)
+  end
 end
